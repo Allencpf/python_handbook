b71271d182136b33a740e9bfbfa8c81ba766aea0
Allen <1151104897@qq.com>
2020-03-11 10:36:00 +0800

    initialize

--- pythonrumen/aliens/bullet.py
+++ pythonrumen/aliens/bullet.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3.7
+
 import pygame
 from pygame.sprite import Sprite
 
--- pythonrumen/datavisual/scatter_squares.py
+++ pythonrumen/datavisual/scatter_squares.py
@@ -1,15 +1,18 @@
 #!/usr/local/bin/python3.7
 import matplotlib.pyplot as plt 
 
-plt.scatter(2, 4, s=200)
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues,edgecolors= 'none',s=40)
 
 
 # 设置图表标题并给坐标轴加上标签
 plt.title("Square Numbers", fontsize = 24)
 plt.xlabel("Value", fontsize=14)
-plt.ylabel("Square of Value", fontszie=14)
+plt.ylabel("Square of Value", fontsize=14)
 
 # 设置刻度标记的大小
-plt.tick_params(axis='both', which='major', labelsize=4)
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
